eaf345b9c0c97e22c497105097d06a35e3f0d6b9
Athina Gouseti <athina.gouseti@outlook.com>
2021-09-10 18:27:51 +0100

    Guest tests

---
+++ tests/guest_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from src.guest import Guest
+
+
+class TestGuest(unittest.TestCase):
+
+    def setUp(self):
+        self.guest = Guest("John", 50, "Hot Blooded")
+
+    def test_guest_has_name(self):
+        self.assertEqual("John", self.guest.name)
+        
+    def test_guest_has_wallet(self):
+        self.assertEqual(50, self.guest.wallet)
+
+    def test_guest_has_favourite_song(self):
+        self.assertEqual("Hot Blooded", self.guest.favourite_song)
